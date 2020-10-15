2ec149994060ba5fd1b49caeb2666dba53444156
Mehmet Taşar <tsrmehmet@hotmail.com>
2020-10-15 12:21:16 +0300

    Urun sınıfı eklendi.

---
+++ OOPRuntime_0/Models/Urun.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPRuntime_0.Models
+{
+    public class Urun: BaseEntity
+    {
+    }
+}
